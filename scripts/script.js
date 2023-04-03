eb404e5e213686728b8a02fe0bea491f953c98ca
Rrantisi <rantisireham19@gmail.com>
2023-04-03 14:30:21 -0400

    wip: creates constants and state variables

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+/*----- constants -----*/
+const valuesRef = ['🦘', '🦎', '🦁', '🦌', '🐹', '🦤', '🐌', '🦈', '🦉', '🐕',
+                    '🐯', '🦄', '🐩', '🐻', '🦙', '🐳', '🦖', '🦝', '🐎', '🦒']
+
+/*----- state variables -----*/
+let seconds;
+let lives;
+let score;
+let firstCard, secondCard;
+let firstCardVal, secondCardVal;
+let cardFrontArray;
+
+/*----- cached elements -----*/
+
+/*----- event listeners -----*/
+
+/*----- functions -----*/
