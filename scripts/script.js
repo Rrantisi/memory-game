ff117c93ea1c40c9a8a82fb9472da5384d716935
Rrantisi <rantisireham19@gmail.com>
2023-04-03 14:37:22 -0400

    wip: creates initialize function

--- scripts/script.js
+++ scripts/script.js
@@ -11,7 +11,23 @@ let firstCardVal, secondCardVal;
 let cardFrontArray;
 
 /*----- cached elements -----*/
-
+const playAgainBtn = document.getElementById('play-again');
+const startGameBtn = document.getElementById('start')
 /*----- event listeners -----*/
 
 /*----- functions -----*/
+initialize()
+function initialize(){
+    seconds = 0;
+    lives = 10;
+    score = 0;
+    firstCard = '';
+    secondCard = '';
+    firstCardVal = '';
+    secondCardVal = '';
+    cardFrontArray = [];
+    render();
+}
+
+function render(){
+}
